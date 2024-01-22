504b2d8d1626bb12949930c44b7d9d0ec04131df
FengXiang2233 <fjx13125@163.com>
2024-01-22 13:20:16 +0800

    fix

--- src/initialize/GetVersionInfo.py
+++ src/initialize/GetVersionInfo.py
@@ -3,4 +3,4 @@ from initialize.g_vars import lanucher
 
 # 获取所有版本列表
 def init_GetAllVerList():
-    lanucher.MinecraftVersions=os.listdir(lanucher.MinecraftPath)
+    lanucher.MinecraftVersions=os.listdir(lanucher.MinecraftPath+"/versions")
